526189f1258f6499977caa9b4318e3758daedaf8
brokerstir <webdev@brokerstir.com>
2017-12-24 22:58:39 -0500

    formatted code

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Robert Risk | Auto Database</title>
-<?php require_once "bootstrap.php"; ?>
+
+    <!-- home page for autocrud assignment by dr. chuck at wa4e.com -->
+    <title>Robert Risk | Auto Database</title>
+    <?php require_once "bootstrap.php"; ?>
+
 </head>
 <body>
-<div class="container">
-<h1>Welcome Automobiles Database</h1>
 
-<p>
-<a href="login.php">Please Log In</a>
-</p>
+    <div class="container">
 
+        <h1>Welcome Automobiles Database</h1>
 
-</div>
+        <p>
+            <a href="login.php">Please Log In</a>
+        </p>
+
+    </div>
+    
 </body>
+</html>
